f100bf90de77930f7bb057d38d65665afb45ef38
nigaya <n.moeko1966@gmail.com>
2022-05-24 21:55:59 +0900

    20220524

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -15,17 +15,23 @@ use Illuminate\Support\Facades\Auth;
 
 class HelloController extends Controller
 {
-    public function index(Request $request){
-        return view('hello.index',['msg'=>'フォームを入力']);
-    }
+  
+   public function index(Request $request)
+   {
+       return view('hello.index', ['msg'=>'フォームを入力：']);
+   }
 
-    public function post(Request $request){
-        $validate_rule = [
-            'name' => 'required',
-            'mail' => 'email',
-            'age' => 'numeric|between:0,150',
-        ];
-        $this->validate($request,$validate_rule);
-        return view('hello.index',['msg'=>'正しく入力されていました！']);
-    }
+
+   public function post(Request $request)
+   {
+       $validate_rule = [
+           'name' => 'required',
+           'mail' => 'email',
+           'age' => 'numeric|between:0,150',
+       ];
+       $this->validate($request, $validate_rule);
+       return view('hello.index', ['msg'=>'正しく入力されました！']);
+   }
+
 }
+
--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -1,23 +1,5 @@
 <html>
 
-{{-- <head>
-    <title>Hello/Index</title>
-    <style>
-        body {
-            font-size: 16pt;
-            color: #999;
-        }
-
-        h1 {
-            font-size: 100pt;
-            text-align: right;
-            color: #f6f6f6;
-            margin: -50px 0px -100px 0px;
-        }
-    </style>
-</head>
-
-<body> --}}
 @extends('layouts.helloapp')
 
 @section('title','Index')
@@ -28,27 +10,20 @@
 @endsection
 
 @section('content')
-<p>{{$msg}}</p>
-<form action="hello" method="post"></form>
-<table>
-    @csrf
-    <tr>
-        <th>name: </th>
-        <td><input type="text" name="name"></td>
-    </tr>
-    <tr>
-        <th>mail: </th>
-        <td><input type="text" name="mail"></td>
-    </tr>
-    <tr>
-        <th>age: </th>
-        <td><input type="text" name="age"></td>
-    </tr>
-    <tr>
-        <th></th>
-        <td><input type="submit" value="send"></td>
-    </tr>
-</table>
+   <p>{{$msg}}</p>
+   <form action="/hello" method="post">
+   <table>
+       @csrf
+       <tr><th>name: </th><td><input type="text" 
+            name="name"></td></tr>
+       <tr><th>mail: </th><td><input type="text" 
+            name="mail"></td></tr>
+       <tr><th>age: </th><td><input type="text" 
+            name="age"></td></tr>
+       <tr><th></th><td><input type="submit" 
+            value="send"></td></tr>
+   </table>
+   </form>
 @endsection
 
 
@@ -57,16 +32,5 @@
 @section('footer')
 huyahuya
 @endsection
-
-
-
-    {{-- <h1>Blade/Index</h1>
-    <p>forディレクティブ</p>
-
-    @php
-        echo 'hello';
-    @endphp --}}
-
-{{-- </body> --}}
 
 </html>
